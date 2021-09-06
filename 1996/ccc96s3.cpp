b8bb9ef18159567854eb8409dc3ec06712394dea
Peter Wang <40186116+PeteMango@users.noreply.github.com>
2021-09-06 16:27:20 -0400

    Update ccc96s3.cpp

--- 1996/ccc96s3.cpp
+++ 1996/ccc96s3.cpp
@@ -1,1 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+int T, N, K;
 
+int main () {
+    cin >> T;
+    for(int i = 1; i <= T; i++) {
+        int pattern[30] = {0};
+        cin >> N >> K;
+        for(int j = 0; j < K; j++) {
+            pattern[j] = 1;
+        }
+        cout << "The bit patterns are\n";
+        do {
+            for(int j = 0; j < N; j++) {
+                cout << pattern[j];
+            }
+            cout << "\n";
+        } while(prev_permutation(pattern, pattern+N));
+        cout << "\n";
+    }
+}
